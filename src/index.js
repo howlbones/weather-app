4ce38603f72ed89c4854033e54649479ef98a828
Howl Kimura <howlkimuradev@gmail.com>
2023-08-19 15:03:36 +0300

    refactor search request and data display to async/await

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import './main.css';
 import { fetchData } from './fetchData';
 
-(function app() {
-  fetchData('search', 'saint petersburg').then((responce) => {
-    const data = responce;
-    console.log('Got search data:');
-    console.log(data);
-  });
-  fetchData('forecast', 'moscow').then((responce) => {
-    const data = responce;
+(async function app() {
+  const searchRequest = 'saint-petersburg';
+  const searchResult = await fetchData('search', searchRequest).then(
+    (result) => {
+      console.log('Got search data:');
+      console.log(result[0].url);
+      console.log(result);
+      console.log('\n');
+      return result;
+    }
+  );
+
+  const weatherData = await fetchData(
+    'forecast',
+    await searchResult[0].url
+  ).then((responce) => {
     console.log('Got forecast data:');
-    console.log(data);
+    console.log(responce);
+    console.log('\n');
+    return responce;
   });
 })();
